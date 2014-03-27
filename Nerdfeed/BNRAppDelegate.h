0d7755550b0095286c6c10c8b690b8846c1965d3
Justin Steffen <JustinDSN@gmail.com>
2014-03-27 11:06:25 -0700

    Initial Commit

---
+++ Nerdfeed/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Nerdfeed
+//
+//  Created by Justin Steffen on 3/27/14.
+//  Copyright (c) 2014 Justin Steffen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
